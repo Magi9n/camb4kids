2e4e7a06077b472da936ef043a0eff9bc4430a5e
ivan <Ivan.ugarten@gmail.com>
2025-07-19 16:13:21 -0500

    feat: configuración centralizada de TypeORM y conexión de entidades

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { RatesModule } from './modules/rates/rates.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
     ScheduleModule.forRoot(),
-    // Aquí se importarán los módulos: AuthModule, RatesModule, OrdersModule, AdminModule
+    AuthModule,
+    RatesModule,
+    OrdersModule,
+    AdminModule,
   ],
 })
 export class AppModule {} 
