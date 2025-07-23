90b7866486dfaa48a376a50611eb9215851e4cf0
ivan <Ivan.ugarten@gmail.com>
2025-07-22 22:27:02 -0500

    pcpp

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,6 +34,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.setGlobalPrefix('api');
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`Aplicación ejecutándose en puerto ${port}`);
