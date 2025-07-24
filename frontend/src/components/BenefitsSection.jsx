3eda40363c3aeb56f5ace7a85560b294209bc2b4
ivan <Ivan.ugarten@gmail.com>
2025-07-24 17:24:49 -0500

    camb

--- frontend/src/components/BenefitsSection.jsx
+++ frontend/src/components/BenefitsSection.jsx
@@ -32,14 +32,14 @@ const BenefitsSection = () => (
         {/* Cuadro 2: Preferencial */}
         <Grid item xs={12} md={4}>
           <Paper elevation={0} sx={{ p: 4, textAlign: 'left', borderRadius: 20, minHeight: 320, minWidth: 320, maxWidth: 340, height: 320, width: '100%', bgcolor: '#baffd7', boxShadow: '0 8px 32px 0 rgba(0,0,0,0.12)', mx: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-            <Typography sx={{ fontFamily: 'Roboto, sans-serif', fontWeight: 700, fontSize: 28, color: '#111', mb: 0.5 }}>
+            <Typography sx={{ fontFamily: 'Roboto, sans-serif', fontWeight: 700, fontSize: 28, color: '#111', mb: 0.1 }}>
               Cupón <span style={{ color: '#179c46', fontWeight: 700, fontFamily: 'Roboto, sans-serif' }}>PREFERENCIAL</span>
             </Typography>
-            <Typography sx={{ fontFamily: 'Roboto, sans-serif', fontWeight: 400, fontSize: 20, color: '#222', mb: 1 }}>
+            <Typography sx={{ fontFamily: 'Roboto, sans-serif', fontWeight: 400, fontSize: 20, color: '#222', mb: 0.5 }}>
               Monto mayor a $10 000 o S/30 000
             </Typography>
             <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 1 }}>
-              <img src={astronautamillonario} alt="Astronauta millonario" style={{ width: 180, height: 180, objectFit: 'contain' }} />
+              <img src={astronautamillonario} alt="Astronauta millonario" style={{ width: 210, height: 210, objectFit: 'contain' }} />
             </Box>
           </Paper>
         </Grid>
@@ -53,7 +53,7 @@ const BenefitsSection = () => (
               Tenemos la mejor tasa de cambio para tu empresa.
             </Typography>
             <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 1 }}>
-              <Lottie animationData={empresario} loop autoplay style={{ width: 180, height: 180 }} />
+              <Lottie animationData={empresario} loop autoplay style={{ width: 210, height: 210 }} />
             </Box>
           </Paper>
         </Grid>
