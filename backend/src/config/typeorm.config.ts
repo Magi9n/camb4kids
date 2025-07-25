97fec3650ec695d9697918efe882527db769a5c2
ivan <Ivan.ugarten@gmail.com>
2025-07-24 19:48:08 -0500

    camb

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -5,6 +5,7 @@ import { User } from '../modules/auth/entities/user.entity';
 import { Order } from '../modules/orders/entities/order.entity';
 import { ExchangeRate } from '../modules/rates/entities/exchange-rate.entity';
 import { AdminSetting } from '../modules/admin/entities/admin-setting.entity';
+import { Alert } from '../modules/alerts/alert.entity';
 
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_USERNAME:', process.env.DB_USERNAME);
@@ -18,7 +19,7 @@ export default new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [User, Order, ExchangeRate, AdminSetting],
+  entities: [User, Order, ExchangeRate, AdminSetting, Alert],
   migrations: ['src/migrations/*.ts'],
   migrationsRun: false,
   synchronize: false,
