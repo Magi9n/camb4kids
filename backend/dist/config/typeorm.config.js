d8ac7bdb85332d642623a3a3eefc10586593aaf7
Ivan <ivan.ugarten@gmail.com>
2025-07-25 04:14:49 +0200

    camb

--- backend/dist/config/typeorm.config.js
+++ backend/dist/config/typeorm.config.js
@@ -3,11 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 dotenv.config();
 const typeorm_1 = require("typeorm");
-const user_entity_1 = require("../modules/auth/entities/user.entity");
-const order_entity_1 = require("../modules/orders/entities/order.entity");
-const exchange_rate_entity_1 = require("../modules/rates/entities/exchange-rate.entity");
-const admin_setting_entity_1 = require("../modules/admin/entities/admin-setting.entity");
-const alert_entity_1 = require("../modules/alerts/alert.entity");
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_USERNAME:', process.env.DB_USERNAME);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
@@ -19,8 +14,8 @@ exports.default = new typeorm_1.DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [user_entity_1.User, order_entity_1.Order, exchange_rate_entity_1.ExchangeRate, admin_setting_entity_1.AdminSetting, alert_entity_1.Alert],
-    migrations: ['dist/migrations/*.js'],
+    entities: [__dirname + '/../modules/**/*.entity.js'],
+    migrations: [__dirname + '/../migrations/*.js'],
     migrationsRun: false,
     synchronize: false,
     logging: process.env.NODE_ENV === 'development',
